685360f8377e0253ce40b6def3927dfb3b4c0243
zhengjinshou <zhengjinshou@huawei.com>
2025-01-14 19:43:05 +0800

    feat: 鸿蒙化空实现

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-'use strict'
+'use strict';
 
-const { NativeModules } = require('react-native');
-const { ImagePickerManager } = NativeModules;
+import ImagePickerManager from './NativeImgPicker';
 
 const DEFAULT_OPTIONS = {
   title: 'Select a Photo',
@@ -14,17 +13,29 @@ const DEFAULT_OPTIONS = {
     title: 'Permission denied',
     text: 'To be able to take pictures with your camera and choose images from your library.',
     reTryTitle: 're-try',
-    okTitle: 'I\'m sure',
-  }
+    okTitle: "I'm sure",
+  },
 };
 
 module.exports = {
-  ...ImagePickerManager,
+  /**
+   * react-native-image-picker
+   */
+  launchCamera: ImagePickerManager.launchCamera,
+  launchImageLibrary: ImagePickerManager.launchImageLibrary,
   showImagePicker: function showImagePicker(options, callback) {
     if (typeof options === 'function') {
       callback = options;
       options = {};
     }
-    return ImagePickerManager.showImagePicker({...DEFAULT_OPTIONS, ...options}, callback)
-  }
-}
+    return ImagePickerManager.showImagePicker({ ...DEFAULT_OPTIONS, ...options }, callback);
+  },
+
+  /**
+   * RN0.61 imagepickerios
+   */
+  canRecordVideos: ImagePickerManager.canRecordVideos,
+  canUseCamera: ImagePickerManager.canUseCamera,
+  openCameraDialog: ImagePickerManager.openCameraDialog,
+  openSelectDialog: ImagePickerManager.openSelectDialog,
+};
